b52b14a46b0805be7090eded9bd7c96ad1ac7cfe
ilkisha <i.shalvarkov@gmail.com>
2026-01-07 17:21:33 +0200

    feat: Add webapp dependencies and improve registration form
    - Add Symfony Asset Mapper, Debug Bundle, and Web Profiler Bundle
    - Add email validation to registration form
    - Add SimpleCSS framework to base template
    - Set up assets directory structure

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -5,19 +5,30 @@ namespace App\Form;
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Email;
 
 class RegistrationFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
+            ->add('email', EmailType::class, [
+                'constraints' => [
+                    new Email([
+                        'message' => 'Please enter a valid email address',
+                    ]),
+                    new NotBlank([
+                        'message' => 'Please enter your email address',
+                    ]),
+                ],
+            ])
             ->add('agreeTerms', CheckboxType::class, [
                 'mapped' => false,
                 'constraints' => [
